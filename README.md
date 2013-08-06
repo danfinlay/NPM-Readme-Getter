70f227bf2c4450b6d04cdb906dea795b5cd1a5d7
flyswatter <somniac@me.com>
2013-08-06 11:56:56 -0700

    Refactored to streaming interface with caching to LevelDB

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 #Readme-Getter
 
-This module will return the HTML of an npm README from within a browser, and return it via a callback.  Just pass the module name:
+This module will return the HTML of an npm README from within a browser, and return it via a writable stream.  Just pass the module name and a writable stream:
 
 `var readmeGetter = require('readme-getter');
-  readmeGetter('moduleName', function(er, readme){
-    if(!er){
-      console.log("Here's your readme: "+readme);
-    }
-  });`
+  readmeGetter('moduleName', console.log);
+`
+
+Utilizes levelDB caching of readme files for quicker retrieval once files have been requested once.  Right now does not feature a shelf-life, so database must be flushed manually.
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 var request = require('request');
-var cheerio = require('cheerio')  
+var through = require('through');
+var trumpet = require('trumpet');
+var level = require('level');
+var db = level('./readme-cache');
 
-module.exports = function(module, callback){
-  var moduleURL = 'http://npmjs.org/'+module;
-  request({url: moduleURL, json:true}, function(er, res, body){
-    // console.log("Err: "+er)
-    if(!er && body){
-      var npmDOM = cheerio.load(body);
-      var normalized = npmDOM('#readme').html();
-      // console.log("Body: "+normalized);
-      callback(er, normalized);
+module.exports = function(module, writeStream){
+	
+	
+	db.get(module, function(er, readme){
+		if(er){		
+	    //Module not cached, load it from web:
+	
+	    var tr  = trumpet();
+      readmeStream = tr.select('#readme').createStream();
+
+	    var moduleURL = 'http://npmjs.org/'+module;
+      var req =	request(moduleURL).pipe(tr);
+      req.pipe(writeStream);
+     
+      var readmeCache = ''; 
+      req.pipe(through(function(data){
+          readmeCache += data.toString();
+        }, function(){
+          db.put(module, readme);
+      })); 
+
     }else{
-      callback(er, null);
+
+      //Module is cached, is now readme:
+      writeStream.end(readme);
     }
-  })
+  });
+
 }
