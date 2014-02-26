299f0de02fa6cd6b54cccba23fa45f1ef1d43197
Dan Finlay <danfinlay@apple.com>
2014-02-25 20:01:10 -0800

    Updated and fixed up for sanity and function.

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 #Readme-Getter
 
-This module will return the HTML of an npm README from within a browser, and return it via a writable stream.  Just pass the module name and a writable stream:
+This module is intended for use with browserify, and will not work in a node.js server.
 
-`
-var http = require('http');
+This module will return the HTML of an npm README, and return it via callback.  Just pass the module name and a callback:
+
+```javascript
 var readmeGetter = require('readme-getter');
 
-http.createServer(function(req, res){
-
-  readmeGetter('moduleName', res);
-}).listen(8000);
-`
-
-Utilizes levelDB caching of readme files for quicker retrieval once files have been requested once.  Right now does not feature a shelf-life, so database must be flushed manually.
+readmeGetter(moduleName, function(er, readme){
+	if(!er) console.log("Readme for %s is:\n %s", moduleName, readme);
+});
+```
--- index.js
+++ index.js
@@ -1,43 +1,28 @@
-var request = require('request');
+var request = require('browser-request');
 var through = require('through');
 var trumpet = require('trumpet');
-var level = require('level');
-var db = level('./readme-cache');
-var domain = require('domain');
 
-module.exports = function(module, writeStream){
-  
-  var d = domain.create();
-  d.on('error', function(er){
-    console.log("Domain caught error: "+er.message);
-  })
-  d.run(function(){
-    db.get(module, function(er, readme){
-      if(er){   
-        //Module not cached, load it from web:
+var readmeCache = {};
+
+module.exports = function(module, callback) {
+
+  if (readmeCache[module]) {
+    callback(null, readmeCache[module]);
+  } else {
+
+    var tr = trumpet();
+    readmeStream = tr.select('#readme').createStream();
+
+    var moduleURL = 'http://npmjs.org/' + module;
+    var req = request(moduleURL).pipe(tr);
+
+    var readme = '';
+    req.pipe(through(function(data) {
+      readme += data.toString();
+    }, function() {
+      readmeCache[module] = readme;
+      callback(null, readme);
+    })).on('error', callback);
     
-        var tr  = trumpet();
-        readmeStream = tr.select('#readme').createStream();
-
-        var moduleURL = 'http://npmjs.org/'+module;
-        var req = request(moduleURL).pipe(tr);
-        readmeStream.pipe(writeStream);
-       
-        var readmeCache = ''; 
-        req.pipe(through(function(data){
-            readmeCache += data.toString();
-          }, function(){
-            // console.log("Attempting to cache "+module+" with readme: "+readmeCache);
-            db.put(module, readme, function(err){
-              // console.log("Problem caching module: "+err);
-            });
-        })); 
-
-      }else{
-
-        //Module is cached, is now readme:
-        writeStream.end(readme);
-      }
-    });
-  })
+  }
 }
